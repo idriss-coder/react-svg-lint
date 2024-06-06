ef7c9faca0af11b35a5c5fdb0260d2ce8136bc1d
IDRISSHACKER <idrisscoder@gmail.com>
2024-06-06 13:35:21 +0100

    Configure npm

---
+++ postinstall.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+// const src = path.join(__dirname, 'target', 'release', 'react_svg_modifier.svg');
+// const dest = path.join(__dirname, 'bin', 'react_svg_modifier.svg');
+//
+// fs.copyFileSync(src, dest);
